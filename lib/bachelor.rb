795a82af2f15a02b30de830492df3ff06da94afc
Princeton Rose <decoyzpr@gmail.com>
2019-03-08 00:31:51 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,22 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
   # code here
+  info = data
+  info.each do |key, value|
+    if key == season
+      value.each do |key2|
+        binding.pry
+        if key2.has_value?("Winner")
+          key2.each do |key3, value2|
+            if key3 == "name"
+              return value2.split(' ').first
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
