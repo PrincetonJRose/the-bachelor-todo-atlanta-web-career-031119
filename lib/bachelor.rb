748650c720624ade5dc7edaea11bec8bec699310
Princeton Rose <decoyzpr@gmail.com>
2019-03-08 00:35:40 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -19,6 +19,18 @@ end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |key, value|
+      value.each do |key2|
+        if key2.has_value?(occupation)
+          key2.each do |key3, value2|
+            if key3 == "name"
+              return value2.split(' ').first
+            end
+          end
+        end
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
